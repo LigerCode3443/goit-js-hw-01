09aa2f30d4ca6f91c7b497879dc7acae07e15795
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-04-17 19:00:53 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,6 @@
 function getElementWidth(content, padding, border) {
-  //   const top = Number.parseFloat(getElementWidth);
-  const box_sizy = parseFloat(content, padding, border);
-  return content + 2 * padding + 2 * border;
+  //   const totalPrice = getElementWidth.parseFloat;
+  return parseFloat(content) + parseFloat(padding) * 2 + 2 * parseFloat(border);
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
